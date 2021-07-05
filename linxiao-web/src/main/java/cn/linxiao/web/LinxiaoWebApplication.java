62bab9e30402b3d0ae90f1e6840bc14d167dc0df
zengzhong@xmiles.cn <zensezzz>
2021-07-05 17:09:24 +0800

    init lingxiao-web

---
+++ linxiao-web/src/main/java/cn/linxiao/web/LinxiaoWebApplication.java
@@ -0,0 +1,13 @@
+package cn.linxiao.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinxiaoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinxiaoWebApplication.class, args);
+	}
+
+}
