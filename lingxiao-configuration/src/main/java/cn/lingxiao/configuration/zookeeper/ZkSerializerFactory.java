36817a40faa00a6fc078fe19681540db5d81e765
zengzhong@xmiles.cn <zensezzz>
2021-07-05 15:39:10 +0800

    init zookeeper serializer

---
+++ lingxiao-configuration/src/main/java/cn/lingxiao/configuration/zookeeper/ZkSerializerFactory.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.lingxiao.configuration.zookeeper;
+
+import cn.lingxiao.configuration.zookeeper.serializer.JavaSerializer;
+import cn.zensezz.lingxiao.util.SpiLoadFactory;
+import org.I0Itec.zkclient.serialize.ZkSerializer;
+
+import java.util.Objects;
+import java.util.ServiceLoader;
+import java.util.stream.StreamSupport;
+
+public class ZkSerializerFactory {
+
+    private static final ServiceLoader<ZkSerializer> SERVICE_LOADER = SpiLoadFactory.loadAll(ZkSerializer.class);
+
+    /**
+     * product  ZkSerializer with className.
+     *
+     * @param className className
+     * @return ZkSerializer
+     */
+    public static ZkSerializer of(final String className) {
+        return StreamSupport.stream(SERVICE_LOADER.spliterator(), false)
+                .filter(service ->
+                        Objects.equals(service.getClass().getName()
+                                        .substring(service.getClass().getName().lastIndexOf(".") + 1),
+                                className)).findFirst().orElse(new JavaSerializer());
+    }
+}
